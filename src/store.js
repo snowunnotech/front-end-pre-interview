4630b01b3b23023d4c477307f986a603490e670b
Lilybon <zebra10029@gmail.com>
2019-07-12 16:32:54 +0800

    Fix loadedToEnd counting bug and create a important message snack bar

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
+import router from './router'
+import { bus } from '@/main'
 
 Vue.use(Vuex)
 
@@ -29,13 +31,14 @@ export default new Vuex.Store({
     },
     UPDATE_BOOK_BY_IDX(state, payload) {
       let book = payload.book, idx = payload.idx
-      state.books[idx] = book
+      if (idx >= state.books.length) state.books.push(book)
+      else state.books[idx] = {...book}
     },
     RESET_LOAD_BOOKS(state) {
       state.booksLoaded = 6
     },
     LOAD_BOOKS(state) {
-      if(!this.getters.loadedToEnd) state.booksLoaded += 6
+      if(!this.getters.loadedToEnd) state.booksLoaded = Math.min(state.booksLoaded + 6, state.books.length)
     },
     SET_DETAIL_ID(state, payload) {
       let id = payload.id
@@ -49,13 +52,14 @@ export default new Vuex.Store({
           .then(res => {
             let books = res.data['hydra:member']
             books.forEach(book => {
+              // 去除@id不必要的字串 /books/, 因其會影響之後replaceBook的put method而且也會使整體的route變得不整潔
               book['@id'] = book['@id'].replace(/\/books\//gi, '')
             })
             commit('UPDATE_BOOKS', {
               books
             })
           })
-          .catch(e => alert(e))
+          .catch(e => bus.$emit('showSnackBar', e))
       }
     },
     replaceBook({ commit, state }, payload) {
@@ -68,11 +72,13 @@ export default new Vuex.Store({
               book,
               idx: state.books.findIndex(book => book['@id'] === state.detailId)
             })
+            bus.$emit('showSnackBar', 'Replace a book successfully!')
+            router.push('/')
           } else {
-            alert(`Replacing a book failed!, status code: ${res.status }`)
+            bus.$emit('showSnackBar', `Replacing a book failed!, status code: ${res.status}`)
           }
         })
-        .catch(e => alert(e))
+        .catch(e => bus.$emit('showSnackBar', e))
     },
     createBook({ commit, state }, payload) {
       let params = payload.params
@@ -82,13 +88,15 @@ export default new Vuex.Store({
             let book = res.data
             commit('UPDATE_BOOK_BY_IDX', {
               book,
-              idx: state.books.length // 最後一個位置更新
+              idx: state.books.length
             })
+            bus.$emit('showSnackBar', 'Create a book successfully!')
+            router.push('/')
           }else{
-            alert(`Creating a book failed!, status code: ${res.status }`)
+            bus.$emit('showSnackBar', `Creating a book failed!, status code: ${res.status}`)
           }
         })
-        .catch(e => alert(e))
+        .catch(e => bus.$emit('showSnackBar', e))
     }
   }
 })
