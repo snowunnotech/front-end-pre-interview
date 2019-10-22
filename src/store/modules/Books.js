5f9606bc9600933acb46041f08d27f47bd841d02
Yu Lo <vikinglo123@gmail.com>
2019-10-22 20:54:15 +0800

    Initial store setup

---
+++ src/store/modules/Books.js
@@ -0,0 +1,10 @@
+export const namespaced = true;
+
+export const state = {
+  books: null
+};
+export const mutations = {};
+export const actions = {};
+export const getters = {
+  getBooks: state => state.books
+};
---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import * as books from "./modules/Books.js";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  plugins: [
+    createPersistedState({
+      key: "Persist"
+    })
+  ],
+  modules: {
+    books
+  },
+  state: {},
+  mutations: {},
+  actions: {}
+});
