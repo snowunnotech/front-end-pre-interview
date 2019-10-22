1d83ab197668b70e44dc0bd89c2d908a2a66bed3
Yu Lo <vikinglo123@gmail.com>
2019-10-22 21:26:29 +0800

    API fetched when list mounted, then stored in books state

--- src/store/modules/Books.js
+++ src/store/modules/Books.js
@@ -1,10 +1,24 @@
 export const namespaced = true;
 
 export const state = {
-  books: null
+  books: []
 };
-export const mutations = {};
-export const actions = {};
+export const mutations = {
+  SET_BOOKS(state, books) {
+    state.books = books;
+  },
+  ADD_BOOKS(state, books) {
+    state.books.push(books);
+  }
+};
+export const actions = {
+  setBooks({ commit }, payload) {
+    commit("SET_BOOKS", payload);
+  },
+  addBooks({ commit }, payload) {
+    commit("ADD_BOOKS", payload);
+  }
+};
 export const getters = {
   getBooks: state => state.books
 };
--- src/views/BookList.vue
+++ src/views/BookList.vue
@@ -1,11 +1,33 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <listedObject></listedObject>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import listedObject from "@/components/BooklistedObject";
+import BooksService from "@/services/BooksService.js";
+import { mapGetters } from "vuex";
 
 export default {
-  components: {}
+  components: {
+    listedObject
+  },
+  async mounted() {
+    try {
+      const response = await BooksService.get({ page: "1" });
+      console.log(response);
+      if (response.status !== 200) {
+        return;
+      } else {
+        this.$store.dispatch("books/setBooks", response.data["hydra:member"]);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  computed: {
+    ...mapGetters("books", ["getBooks"])
+  }
 };
 </script>
