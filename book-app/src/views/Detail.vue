5077c600c1e8e5afecb998e10eb8dafe8c62afb5
Skanda lin <anikiwayne@gmail.com>
2019-12-06 10:55:39 +0800

    added pages

---
+++ book-app/src/views/Detail.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="detail">
+      <bookDetail/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import bookDetail from '@/components/books/bookDetail/bookDetail'
+
+export default {
+  name: 'detail',
+  components: {
+    bookDetail
+  },
+  methods: {
+  },
+  mounted() {
+  }
+}
+</script>
---
+++ book-app/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <addBook/>
+    <books/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import books from '@/components/books/books'
+import addBook from '@/components/books/addBook/addBook'
+
+export default {
+  name: 'home',
+  components: {
+    books,
+    addBook
+  },
+  methods: {
+  },
+  mounted() {
+  }
+}
+</script>
