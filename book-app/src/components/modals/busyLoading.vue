952dd2d42cd33a1e349d11119faa729ad32be792
Skanda lin <anikiwayne@gmail.com>
2019-12-06 10:54:35 +0800

    added busy loading modals

---
+++ book-app/src/components/modals/busyLoading.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="busyLoading">
+    <div class="content">
+        <img src="~@/assets/images/loading.gif" alt="">
+    </div>
+</div>
+</template>
+<script>
+    export default {
+        name: 'busyLoading'
+    }
+</script>
