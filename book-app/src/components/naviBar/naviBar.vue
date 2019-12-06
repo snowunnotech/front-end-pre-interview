2df86b2ba04ee3b91e706e60991242ed84087cc3
Skanda lin <anikiwayne@gmail.com>
2019-12-06 10:52:57 +0800

    added navbar com

---
+++ book-app/src/components/naviBar/naviBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="naviBar">
+        <nav class="bg-dark navbar-dark">
+            <div @click="redirect('/')">
+                <a class="navbar-brand" href="#">BookStore</a>
+            </div>
+            <div @click="redirect('/')">
+                <i class="fa fa-chevron-left" aria-hidden="true"></i>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'naviBar',
+    data() {
+        return  {
+        }
+    },
+    methods: {
+        redirect(url) {
+            console.log(url)
+            this.$router.push(url)
+        },
+    }
+}
+</script>
